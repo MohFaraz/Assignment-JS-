366cec00065e09980465bb6ac9a4ba665fc1f591
MohFaraz <mohdfaraz9808@gmail.com>
2023-12-10 23:37:31 +0530

    table using function

---
+++ index.js
@@ -0,0 +1,31 @@
+// Table By using call by value function
+ /*value(10);
+ function value(a)
+ {
+ for( let i=1;i<=a;i++)
+ {
+   console.log(i+'*'+i+'='+i*i);
+ }
+ }*/
+
+/*Table By using call by refrence function
+ var refrence=function(a)
+ {
+ for(let i=1;i<=a;i++)
+ {
+     console.log(i+'*'+i+'='+i*i);
+ }
+ }
+ refrence(10);*/
+
+
+// Table By using call by arrow function
+
+/*var arrow=(a)=>{
+    for(let i=1;i<=a;i++)
+{
+    console.log(i+'*'+i+'='+i*i);
+}
+
+}
+arrow(10);*/
